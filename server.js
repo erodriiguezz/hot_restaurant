04de505f90b48ec05c61279201742913e501a9ac
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-03-11 20:43:32 -0400

    first version updated

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+const currentReservations = [
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  }
+];
+
+const waitList = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(__dirname + "/public/reserve.html");
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(__dirname + "/public/tables.html");
+});
+
+app.post("/api/tables", function(req, res) {
+  const newReservation = req.body;
+  console.log(newReservation);
+
+  if (currentReservations.lenght < 5) {
+    currentReservations.push(newReservation);
+    res.send(true);
+  } else {
+    waitList.push(newReservation);
+    res.send(false);
+  }
+});
+
+app.get("/api/tables", function(req, res) {
+  res.json(currentReservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  res.json(waitList);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening to port 3000");
+});
---
+++ storage/currentReservation.js
@@ -0,0 +1,32 @@
+const currentReservations = [
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  },
+  {
+    customerName: "sdfsdf",
+    phoneNumber: "sdfsdfsd",
+    customerEmail: "sdfsdfsdf",
+    customerID: "sdfsdfsdf"
+  }
+];
